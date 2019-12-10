39b5103bdd97853915a35212d196618db43ecea7
Ryszard Gurzeda <ryszard.gurzeda@netguru.co>
2019-12-10 13:42:02 +0100

    Use generic dropdowns

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
+import userDropdown from "./userDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = userDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = userDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
@@ -20,39 +21,8 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed{" "}
-          <select
-            id="breed"
-            value={breed}
-            onChange={e => setBreed(e.target.vaue)}
-            onBlur={e => setBreed(e.target.vaue)}
-            disabled={!breeds.length}
-          >
-            <option>ALL</option>
-            {breeds.map(breedString => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
