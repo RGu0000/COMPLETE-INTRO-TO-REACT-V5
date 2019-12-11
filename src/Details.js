6370e91ad075e3dbda53aa1a0e5b21e020a53c33
Ryszard Gurzeda <ryszard.gurzeda@netguru.co>
2019-12-11 12:44:29 +0100

    Allow prototype JS calls

--- src/Details.js
+++ src/Details.js
@@ -2,12 +2,7 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  }
+  stete = { loading: true };
   componentDidMount() {
     pet.animal(parseInt(this.props.id)).then(({ animal }) => {
       this.setState({
