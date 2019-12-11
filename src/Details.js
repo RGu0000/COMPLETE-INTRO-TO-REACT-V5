7f820a1caa36581acdde8463fe677e0cab2e4e1d
Ryszard Gurzeda <ryszard.gurzeda@netguru.co>
2019-12-11 13:21:39 +0100

    Add carousel

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,10 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import CArousel from "./Carousel";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
-  stete = { loading: true };
+  state = { loading: true };
   componentDidMount() {
     pet.animal(parseInt(this.props.id)).then(({ animal }) => {
       this.setState({
@@ -20,9 +22,11 @@ class Details extends React.Component {
     if (this.state.loading) {
       return <h1>loading...</h1>;
     }
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
+
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
