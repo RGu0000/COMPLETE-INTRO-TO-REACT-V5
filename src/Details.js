d721ca0a8138320b11dbd945b6514d458d5aba25
Ryszard Gurzeda <ryszard.gurzeda@netguru.co>
2019-12-11 13:44:00 +0100

    Add ErrorBoundary

--- src/Details.js
+++ src/Details.js
@@ -2,10 +2,12 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import CArousel from "./Carousel";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   state = { loading: true };
   componentDidMount() {
+    throw new Error("lol");
     pet.animal(parseInt(this.props.id)).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -38,4 +40,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
